3d0220de33ac8ce1adec29c4327712f4b3c6ec0d
xiaozhou <zxh04551@163.com>
2021-12-10 18:52:30 +0800

    todoList完成

--- react/react脚手架/my-app/src/App.jsx
+++ react/react脚手架/my-app/src/App.jsx
@@ -1,76 +1,10 @@
-import React, { Component } from "react"
-
-import "./App.css"
-
-import Footer from "./components/Footer"
-import Header from "./components/Header/index"
-import List from "./components/List"
+import React, { Component } from 'react'
 
 export default class app extends Component {
-  state = {
-    todos: [
-      { id: 1, name: "吃饭", done: true },
-      { id: 2, name: "睡觉", done: true },
-      { id: 3, name: "玩游戏", done: false },
-      { id: 4, name: "逛街", done: true }
-    ]
-  }
-
-  addTodo = todoObj => {
-    const { todos } = this.state
-    const newTodos = [todoObj, ...todos]
-    this.setState({ todos: newTodos })
-  }
-
-  updateTodo = (id, checkd) => {
-    const { todos } = this.state
-    const newTodos = todos.map(todo => {
-      if (todo.id === id) {
-        // 拷贝并重新赋值
-        return { ...todo, done: checkd }
-      } else {
-        return todo
-      }
-    })
-    this.setState({ todos: newTodos })
-  }
-
-  delectTodo = id => {
-    const { todos } = this.state
-    const index = todos.findIndex(item => id === item.id)
-    todos.splice(index, 1)
-    this.setState({ todos })
-  }
-
-  handleAllCheck = (done) => {
-    let { todos } = this.state
-    todos = todos.map(item => {
-      return {...item, done}
-    })
-    this.setState({ todos })
-  }
-
-  delectDoneTodo = () => {
-    let { todos } = this.state
-    todos = todos.filter(item => item.done === false)
-    this.setState({ todos })
-  }
-
   render() {
-    const { todos } = this.state
     return (
       <div>
-        <div className="todo-container">
-          <div className="todo-wrap">
-            <Header addTodo={this.addTodo} />
-            <List
-              todos={todos}
-              updateTodo={this.updateTodo}
-              delectTodo={this.delectTodo}
-            />
-            <Footer todos={todos} handleAllCheck={this.handleAllCheck} delectDoneTodo={this.delectDoneTodo} />
-          </div>
-        </div>
+        123
       </div>
     )
   }
