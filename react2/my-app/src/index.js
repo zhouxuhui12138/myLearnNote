1d7321d88be930adc15abf3e26d75c8cc15c1721
xiaozhou <zxh04551@163.com>
2022-04-05 19:06:01 +0800

    commit

--- react2/my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-
-import App from './01-base/03-组件嵌套.jsx'
-
-const container = document.getElementById('root')
-const root = createRoot(container)
-
-root.render(<App />)
