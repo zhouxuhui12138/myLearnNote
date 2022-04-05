1acd62763b1e8eadba816d8431fc411cea3e3222
xiaozhou <zxh04551@163.com>
2022-04-05 13:52:38 +0800

    组件

--- react2/my-app/src/index.js
+++ react2/my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './01-base/02-函数式组件.jsx'
+import App from './01-base/03-组件嵌套.jsx'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
